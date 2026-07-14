217b15b384efc0f57a08b7f5a93eb812ee4ee9f8
secless-fm <fruitlemon.fm@gmail.com>
2026-07-14 14:50:27 +0900

    変数名統一

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ static float deg_radian(float degree)
 class Parts
 {
 public:
-  int pin = -1;
-  bool number_parts = 0;
+  int _pin = -1;
+  bool _number_parts = 0;
 };
 Parts b1; // 0ピン
 Parts b2; // 1ピン
@@ -21,31 +21,36 @@ const int last_pin = 16;
 class Line
 {
 public:
-  int pin;
-  int val;
-  int deg;
-  bool caught;
+  int _pin;
+  int _val;
+  int _deg;
+  bool _caught;
 };
 Line angelLine[last_pin];
 
-int Line_sensorpin[last_pin] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+// i = 3;
+// angelLine[3]
+// angelLine[3]._pin angelLine[3].
+// angelLine[i]._pin angelLine[i]._val
+
+int line_sensorpin[last_pin] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 
 const int recieve_pin = 5;
-const int Line_border = 750;
+const int line_border = 750;
 
 void setup()
 {
   Serial.begin(115200);
 
-  b1.pin = 1;
-  b2.pin = 2;
-  b3.pin = 3;
-  b4.pin = 4;
+  b1._pin = 1;
+  b2._pin = 2;
+  b3._pin = 3;
+  b4._pin = 4;
 
-  pinMode(b1.pin, OUTPUT);
-  pinMode(b2.pin, OUTPUT);
-  pinMode(b3.pin, OUTPUT);
-  pinMode(b4.pin, OUTPUT);
+  pinMode(b1._pin, OUTPUT);
+  pinMode(b2._pin, OUTPUT);
+  pinMode(b3._pin, OUTPUT);
+  pinMode(b4._pin, OUTPUT);
   pinMode(recieve_pin, INPUT);
 }
 
@@ -53,39 +58,67 @@ void loop()
 {
   for (int i = 0; i < last_pin; i++)
   {
-    angelLine[i].pin = Line_sensorpin[i];
-    angelLine[i].deg = i * 30;
+    angelLine[i]._pin = line_sensorpin[i];
+    angelLine[i]._deg = i * 30;
   }
 
   // マルチプレクサ行き
   for (int i = 0; i < last_pin; i++)
   {
-    b1.number_parts = (angelLine[i].pin >> 0) & 0b0001; // (i >> x):iを右に一つずらし、0以下のものを消去 &:かつ。一桁目が一致していたら0b0001ｓ(正)になる
-    b2.number_parts = (angelLine[i].pin >> 1) & 0b0001;
-    b3.number_parts = (angelLine[i].pin >> 2) & 0b0001;
-    b4.number_parts = (angelLine[i].pin >> 3) & 0b0001;
+    b1._number_parts = (angelLine[i]._pin >> 0) & 0b0001; // (i >> x):iを右に一つずらし、0以下のものを消去 &:かつ。一桁目が一致していたら0b0001ｓ(正)になる
+    b2._number_parts = (angelLine[i]._pin >> 1) & 0b0001;
+    b3._number_parts = (angelLine[i]._pin >> 2) & 0b0001;
+    b4._number_parts = (angelLine[i]._pin >> 3) & 0b0001;
 
-    digitalWrite(b1.pin, b1.number_parts);
-    digitalWrite(b2.pin, b2.number_parts);
-    digitalWrite(b3.pin, b3.number_parts);
-    digitalWrite(b4.pin, b4.number_parts);
+    digitalWrite(b1._pin, b1._number_parts);
+    digitalWrite(b2._pin, b2._number_parts);
+    digitalWrite(b3._pin, b3._number_parts);
+    digitalWrite(b4._pin, b4._number_parts);
 
-    angelLine[angelLine[i].pin].val = analogRead(recieve_pin);
+    delayMicroseconds(20);
 
-    Serial.print(angelLine[i].val);
+    angelLine[angelLine[i]._pin]._val = analogRead(recieve_pin);
+
+    Serial.print(angelLine[i]._val);
     Serial.print(", ");
+
+    // // 送信側
+    // uint16_t data = 346;
+    // uint8_t d1 = uint8_t(data & 0b11111111);
+    // uint8_t d2 = uint8_t((data >> 8) & 0b11111111);
+    // Serial.write(d1);
+    // Serial.write(d2);
+
+    // Serial.println(String(data));
+
+    // if (Serial.available())
+    // {
+    //   String a = Serial.readStringUntil('/n');
+    //   uint16_t b = a.toInt();
+    // }
+
+    // // 受信側
+    // if (Serial.available() >= 2)
+    // {
+    //   uint8_t a = Serial.read();
+    //   uint8_t b = Serial.read();
+
+    //   uint16_t fixed_data = (uint16_t(b) << 8) | uint16_t(a);
+    // }
   }
 
   Serial.println("");
 
+  // "abc"
+
   // 反応しているか否かを正負で表記
   for (int i = 0; i < last_pin; i++)
   {
-    angelLine[i].caught = 0;
-    if (angelLine[angelLine[i].pin].val > Line_border)
-      angelLine[angelLine[i].pin].caught = 1;
+    angelLine[i]._caught = 0;
+    if (angelLine[angelLine[i]._pin]._val > line_border)
+      angelLine[angelLine[i]._pin]._caught = 1;
 
-    Serial.print(angelLine[i].caught);
+    Serial.print(angelLine[i]._caught);
     Serial.print(", ");
   }
   Serial.println("");
@@ -94,10 +127,10 @@ void loop()
   float vx = 0.0, vy = 0.0;
   for (int i = 0; i < last_pin; i++)
   {
-    if (angelLine[angelLine[i].pin].caught == 1)
+    if (angelLine[angelLine[i]._pin]._caught == 1)
     {
-      float w = angelLine[angelLine[i].pin].val;
-      float ang = deg_radian(angelLine[angelLine[i].pin].deg);
+      float w = angelLine[angelLine[i]._pin]._val;
+      float ang = deg_radian(angelLine[angelLine[i]._pin]._deg);
       vx += w * cos(ang);
       vy += w * sin(ang);
     }
@@ -111,17 +144,22 @@ void loop()
     unity = vy / vr;
   }
 
-  Serial.print("vx:"); Serial.print(vx);
-  Serial.print(" vy:"); Serial.print(vy);
-  Serial.print(" vr:"); Serial.print(vr);
+  Serial.print("vx:");
+  Serial.print(vx);
+  Serial.print(" vy:");
+  Serial.print(vy);
+  Serial.print(" vr:");
+  Serial.print(vr);
   // Serial.print(" unitx:"); Serial.print(unitx);
   // Serial.print(" unity:"); Serial.print(unity);
 
   float normalx = unity;
   float normaly = -unitx;
 
-  Serial.print(" normalx:"); Serial.print(normalx);
-  Serial.print(" normaly:"); Serial.print(normaly);
+  Serial.print(" normalx:");
+  Serial.print(normalx);
+  Serial.print(" normaly:");
+  Serial.print(normaly);
 
   Serial.println("");
 
